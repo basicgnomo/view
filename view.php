6706b3d036e3ac7424ee218332ad04cdb13a210f
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-09-06 09:51:56 -0300

    @aicouto committed

--- view.php
+++ view.php
@@ -8,7 +8,11 @@ function view($nomeStr,$data=null,$print=true){
     header("HTTP/1.0 404 Not Found");
   }
   $str=ROOT.'call/callgnomo/'.$nomeDoRepositorioStr;
-  //$str=ROOT.$nomeDoRepositorioStr;
+  if(isset($_ENV['CALL'],$_ENV['SITE_MODE'])){
+    if($_ENV['SITE_MODE']=='dev'){
+      $str=ROOT.$nomeDoRepositorioStr;
+    }
+  }
   $str.='/view/'.$nomeDoPacoteStr.'.php';
   if(file_exists($str)){
     if(is_array($data)){
